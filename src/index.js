f8717ff51e70a2372e782c9593ddd2e6b09c3b5c
Wilson Wu <wuyecheng738@gmail.com>
2023-01-17 23:36:28 -0500

    dynamically update url

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 const csvtojson = require('csvtojson');
 import Example from './scripts/example';
 document.addEventListener("DOMContentLoaded", () => {
-  let stock_ticker = 'AAPL';
-  let url2 = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${stock_ticker}&apikey=N7AW4I5CEJEADDLD`;
-  let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=60min&slice=year1month1&apikey=N7AW4I5CEJEADDLD`;
-  fetch(url2)
-    .then(res => res.json())
-    .then(data => { 
-      let stockData = data
-      document.querySelector("#last_quote").innerText = JSON.stringify(stockData)
-      console.log(stockData["Monthly Time Series"]['2023-01-17']["4. close"])
-      console.log(stockData)
-    })
-
-  const stockData = fetch(url)
-    .then(res => res.text())
-    .then(csv => { 
-      csvtojson().fromString(csv)
-      .then(jsonObj => {
-        console.log(jsonObj)
-
+  document.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const stockPick = document.querySelector('input[name="tickers"]:checked').value
+    console.log(stockPick)
+    let url = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${stockPick}&apikey=N7AW4I5CEJEADDLD`;
+    console.log(url)
+    fetch(url)
+      .then(res => res.json())
+      .then(data => { 
+        let stockData = data["Monthly Time Series"]['2023-01-17']["4. close"]
+        document.querySelector("#last_quote").innerText = stockData
+      // console.log(stockData["Monthly Time Series"]['2023-01-17']["4. close"])
       })
-      // let stockData = csvtojson(csv)
-      // console.log(stockData)
-      // document.querySelector("#last_quote").innerText = JSON.stringify(stockData)
-    })
+  })
+  //this fetch the CSV format of the intraday data.
+  // let url2 = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=60min&slice=year1month1&apikey=N7AW4I5CEJEADDLD`;
+  // const stockData = fetch(url2) //
+  //   .then(res => res.text())
+  //   .then(csv => { 
+  //     csvtojson().fromString(csv)
+  //     .then(jsonObj => {
+  //       console.log(jsonObj)
+  //     })
+  //   })
   
-  console.log(JSON.stringify(stockData))
+  
 })
 
 
